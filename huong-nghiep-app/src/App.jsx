178c42d3a29bf7cc9af2730c0679c8833182904d
HuongNghiep360 <user@huongnghiep360.com>
2026-07-24 14:56:15 +0700

    Update footer: Thanh Vu - THPT Ca Mau

--- huong-nghiep-app/src/App.jsx
+++ huong-nghiep-app/src/App.jsx
@@ -82,7 +82,7 @@ function App() {
           </div>
 
           <div className="footer-bottom">
-            <p>&copy; 2026 HuongNghiep360 | Phục vụ học sinh THPT</p>
+            <p>&copy; 2026 HuongNghiep360 | Thanh Vũ - THPT Cà Mau - 2026</p>
           </div>
         </footer>
       </div>
