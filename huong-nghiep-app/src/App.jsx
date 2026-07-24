195fc56a71aa4745fc5fa08f3ba4a8ec8cefc110
HuongNghiep360 <user@huongnghiep360.com>
2026-07-24 15:05:02 +0700

    Replace logo with school logo

--- huong-nghiep-app/src/App.jsx
+++ huong-nghiep-app/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <div className="app">
         <nav className="navbar" aria-label="Điều hướng chính">
           <NavLink to="/" className="logo" end>
-            <span className="logo-mark">🎓</span>
+            <span className="logo-mark"><img src="/logo.png" alt="HuongNghiep360" /></span>
             <span>HuongNghiep360</span>
           </NavLink>
           <div className="nav-links">
